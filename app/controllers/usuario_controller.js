f8c38d12282d524dd338e9355496cf4d753e1792
MarianoBF <mariano.b.fernandez@gmail.com>
2021-04-06 12:49:34 -0300

    password hash & token on login

--- app/controllers/usuario_controller.js
+++ app/controllers/usuario_controller.js
@@ -1,5 +1,8 @@
 const db = require("../models");
 const Usuario = db.usuario;
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../config/db.config");
 
 exports.create = (req, res) => {
   try {
@@ -8,7 +11,7 @@ exports.create = (req, res) => {
       apellido: req.body.apellido,
       email: req.body.email,
       perfil: req.body.perfil,
-      password: req.body.password
+      password: bcrypt.hashSync(req.body.password, 5)
     });
     usuario.save(usuario)
     .then(data => res.send(data))
@@ -23,24 +26,47 @@ exports.create = (req, res) => {
   }
 };
 
-exports.login = (req, res) => {
+  exports.login = (req, res) => {
     try {
-      const usuario = new Usuario({
+      const usuarioLoguear = {
         email: req.body.email,
-        password: req.body.password
-      });
-      usuario.findOne({ email: usuario.email })
-      .then(data => res.send(data))
-      .catch(error => console.log(error))
-  
-    } catch {
+        password: req.body.password,
+      };
+      console.log(usuarioLoguear)
+      Usuario.findOne({ email: usuarioLoguear.email })
+      .then(data => 
+        {        console.log(data)
+
+            const passwordOK = bcrypt.compareSync(usuarioLoguear.password, data.password);
+            if (passwordOK) {
+                const token = jwt.sign(
+                  {
+                    perfil: data.perfil,
+                    email: data.email,
+                    id_usuario: data._id,
+                  },
+                  config.SECRETO,
+                  { expiresIn: 86400 }
+                );
+                res.send(token);
+              } else {
+                res
+                  .status(400)
+                  .send(
+                    "Hubo un problema al loguear, revise los datos y vuelva a intentar en un momento"
+                  );
+              }
+        })
+        .catch(error => console.log(error))         
+        } catch {
       res
         .status(400)
         .send(
-          "Error"
+          "Hubo un problema al loguear, revise los datos y vuelva a intentar en un momento"
         );
     }
   };
+  
 
 
 exports.findAll = (req, res) => {
